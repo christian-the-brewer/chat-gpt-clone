1960622ca94c95f0a50e19cc235568f471a2c7d4
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-05-04 15:32:31 +0000

    adds hero icons

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+import { link } from "fs";
+import { title } from "process";
+
+export default function Head() {
+    return (
+        <>
+        <title>ChatGPT</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
