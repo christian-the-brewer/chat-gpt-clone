eb93d0cd0aed6f5b3aaa045324bd0580011b8c8c
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-05-05 13:10:11 +0000

    add logo

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,10 +1,19 @@
 'use client'
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import Logo from '@component/public/chatgptlogo.png'
 
 function Login() {
     return (
-        <div>Login</div>
+        <div>
+            <Image 
+                src={Logo}
+                width={300}
+                height={300}
+                alt='logo'
+            />
+            <button>Sign In to use ChatGPT</button>
+        </div>
     )
 }
 
